f7a062dfb3466f273ab4b0be6772fd9d467cccbe
Renjith Vincent <renjithkv123@gmail.com>
2021-03-14 23:44:40 +0800

    Create actionbuilder methods and tests

--- lib/robot/robot_action_builder.rb
+++ lib/robot/robot_action_builder.rb
@@ -1,5 +1,39 @@
 module RobotSimulator
   class RobotActionBuilder
+    attr_accessor
+    def initialize
+      @command_parser = RobotSimulator::CommandParser.new
+      @robot = RobotSimulator::Robot.new
+      @table = RobotSimulator::Table.new
+    end
 
+    def execute_actions(command_file_name)
+      action_list = get_action_list(command_file_name)
+      return if action_list.empty?
+      perform_actions_list(action_list)
+    end
+
+    def perform_actions_list(action_list)
+      current_position = @table.initial_position
+      action_list.each do |action|
+        if action.first == @table.place_command
+          current_position = @robot.perform_robot_movement(action).call(action)
+        else
+          current_position = @robot.perform_robot_movement(action).call(current_position)
+        end
+      end
+      current_position
+    end
+
+    private
+
+    def get_action_list(command_file_name)
+      input_command_arr = []
+      File.open(command_file_name, "r").each_line do |line|
+        data = line.split(/\n/)
+        input_command_arr << data
+      end
+      input_command_list = @command_parser.parse_commands(input_command_arr)
+    end
   end
 end
--- spec/robot/robot_action_builder_spec.rb
+++ spec/robot/robot_action_builder_spec.rb
@@ -7,5 +7,23 @@ module RobotSimulator
         expect(RobotActionBuilder).to be RobotSimulator::RobotActionBuilder
       end
     end
+
+    subject {RobotActionBuilder.new}
+    let!(:actions_list) {[["PLACE", "3", "0", "NORTH"], ["MOVE"], ["LEFT"], ["REPORT"], ["RIGHT"],
+      ["PLACE", "3", "3", "SOUTH"], ["MOVE"], ["MOVE"], ["REPORT"], ["RIGHT"], ["REPORT"], ["REPORT"]]}
+
+    context "#perform_actions_list with valid input" do
+      it "should perform all the actions" do
+        expect(subject.perform_actions_list(actions_list)).to eq([3, 1, "WEST"])
+      end
+    end
+
+    context "#execute_actions from input file name" do
+      it "should perform all the actions and return the final position" do
+        input_file_path = File.expand_path(File.expand_path '../../../run_robot/input_files/command_list.txt', __FILE__)
+        expect(subject.execute_actions(input_file_path)).to eq([3, 1, "WEST"])
+      end
+    end
+
   end
 end
