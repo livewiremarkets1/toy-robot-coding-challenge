20dc3aeb9bb17cd1b1b2eb7140ce56213a48ed9a
Renjith Vincent <renjithkv123@gmail.com>
2021-03-14 20:27:20 +0800

    Add coordinates move/turn and tests

--- lib/robot/coordinates.rb
+++ lib/robot/coordinates.rb
@@ -1,5 +1,32 @@
 module RobotSimulator
   class Coordinates
-
+    def get_coordinates(direction, action_val)
+      {
+          :NORTH => {
+              move_x: 0,
+              move_y: 1,
+              left: 'WEST',
+              right: 'EAST'
+          },
+          :SOUTH => {
+              move_x: 0,
+              move_y: -1,
+              left: 'EAST',
+              right: 'WEST'
+          },
+          :WEST => {
+              move_x: -1,
+              move_y: 0,
+              left: 'SOUTH',
+              right: 'NORTH'
+          },
+          :EAST => {
+              move_x: 1,
+              move_y: 0,
+              left: 'NORTH',
+              right: 'SOUTH'
+          }
+      }[direction][action_val]
+    end
   end
 end
--- spec/robot/coordinates_spec.rb
+++ spec/robot/coordinates_spec.rb
@@ -2,9 +2,64 @@ require 'spec_helper'
 
 module RobotSimulator
   describe Coordinates do
+    subject {Coordinates.new}
     context "#initialize" do
       it "should be a part of RobotSimulator module" do
         expect(Coordinates).to be RobotSimulator::Coordinates
+      end
+    end
+
+    context 'turn robot in various directions' do
+      it 'north facing robot left' do
+        expect(subject.get_coordinates(:NORTH, :left)).to eq('WEST')
+      end
+      it 'north facing robot right' do
+        expect(subject.get_coordinates(:NORTH, :right)).to eq('EAST')
+      end
+      it 'south facing robot left' do
+        expect(subject.get_coordinates(:SOUTH, :left)).to eq('EAST')
+      end
+      it 'south robot right' do
+        expect(subject.get_coordinates(:SOUTH, :right)).to eq('WEST')
+      end
+      it 'east facing robot left' do
+        expect(subject.get_coordinates(:EAST, :left)).to eq('NORTH')
+      end
+      it 'east facing robot right' do
+        expect(subject.get_coordinates(:EAST, :right)).to eq('SOUTH')
+      end
+      it 'west facing robot left' do
+        expect(subject.get_coordinates(:WEST, :left)).to eq('SOUTH')
+      end
+      it 'west facing robot right' do
+        expect(subject.get_coordinates(:WEST, :right)).to eq('NORTH')
+      end
+    end
+
+    context 'move robot one unit in various directions' do
+      it 'north facing robot x' do
+        expect(subject.get_coordinates(:NORTH, :move_x)).to eq(0)
+      end
+      it 'north facing robot y' do
+        expect(subject.get_coordinates(:NORTH, :move_y)).to eq(1)
+      end
+      it 'south facing robot x' do
+        expect(subject.get_coordinates(:SOUTH, :move_x)).to eq(0)
+      end
+      it 'south facing robot y' do
+        expect(subject.get_coordinates(:SOUTH, :move_y)).to eq(-1)
+      end
+      it 'east facing robot x' do
+        expect(subject.get_coordinates(:EAST, :move_x)).to eq(1)
+      end
+      it 'east facing robot y' do
+        expect(subject.get_coordinates(:EAST, :move_y)).to eq(0)
+      end
+      it 'west facing robot x' do
+        expect(subject.get_coordinates(:WEST, :move_x)).to eq(-1)
+      end
+      it 'west facing robot y' do
+        expect(subject.get_coordinates(:WEST, :move_y)).to eq(0)
       end
     end
   end
