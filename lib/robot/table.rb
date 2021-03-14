508fbdb3bc285e3e1803c18022e09e274c149ada
Renjith Vincent <renjithkv123@gmail.com>
2021-03-14 21:12:30 +0800

    Add table methods and tests

--- lib/robot/table.rb
+++ lib/robot/table.rb
@@ -1,5 +1,48 @@
 module RobotSimulator
   class Table
+    attr_accessor :place_command, :initial_position
+    def initialize(options={})
+      default_table_width, default_table_height = 5, 5
+      @table_width = options[:width] || default_table_width
+      @table_height = options[:height] || default_table_height
+      @valid_actions = %w(PLACE MOVE LEFT RIGHT REPORT)
+      @valid_directions = %w(NORTH SOUTH EAST WEST)
+      @place_command = 'PLACE'
+      @initial_position = [0,0,'NORTH']
+    end
+
+    def is_within_table?(coordinates_array)
+      return false unless is_valid_array? coordinates_array
+      return false unless coordinates_array.all? {|i| is_integer_string? i.to_s }
+      coordinates_array = coordinates_array.map(&:to_i)
+      x_axis, y_axis = coordinates_array[0], coordinates_array[1]
+      x_axis.between?(0, @table_width - 1) && y_axis.between?(0, @table_height - 1)
+    end
+
+    def is_valid_place?(place_command)
+      return false unless is_valid_array?place_command
+      return false unless place_command.length == 4
+      return false unless place_command.first == @place_command
+      return false unless is_valid_direction? place_command.last
+      place_coordinates = place_command[1..2]
+      is_within_table? place_coordinates
+    end
+
+    def is_valid_action?(action)
+      @valid_actions.include? action
+    end
+
+    def is_valid_direction?(direction)
+      @valid_directions.include? direction
+    end
+
+    def is_valid_array?(array)
+      array && array.kind_of?(Array)
+    end
+
+    def is_integer_string?(string)
+      /\A[-+]?\d+\z/ === string
+    end
 
   end
 end
--- spec/robot/table_spec.rb
+++ spec/robot/table_spec.rb
@@ -7,5 +7,92 @@ module RobotSimulator
         expect(Table).to be RobotSimulator::Table
       end
     end
+
+    subject {Table.new}
+    let!(:valid_directions) {%w(NORTH SOUTH EAST WEST)}
+    let!(:invalid_directions) {%w(NORTH-EAST this_way further_ahead yonder)}
+    let!(:valid_actions) { %w(PLACE MOVE LEFT RIGHT REPORT) }
+    let!(:invalid_actions) { %w(lalalal hahaha lol_man jejeje rofl) }
+
+    context "check valid directions" do
+      it "should return true" do
+        valid_directions.each do |dir|
+          expect(subject.is_valid_direction? dir).to be true
+        end
+      end
+      it "should return false" do
+        invalid_directions.each do |dir|
+          expect(subject.is_valid_direction? dir).to be false
+        end
+      end
+    end
+
+    context "check valid actions" do
+      it "should return true" do
+        valid_actions.each do |action|
+          expect(subject.is_valid_action? action).to be true
+        end
+      end
+      it "should return false" do
+        invalid_actions.each do |action|
+          expect(subject.is_valid_action? action).to be false
+        end
+      end
+    end
+
+    context "check coordinates are within table" do
+      it "should return true" do
+        expect(subject.is_within_table?([0,3])).to be true
+      end
+      it "should return true" do
+        expect(subject.is_within_table?([0,4])).to be true
+      end
+      it "should return true" do
+        expect(subject.is_within_table?([2,3])).to be true
+      end
+      it "should return true" do
+        expect(subject.is_within_table?([1,4])).to be true
+      end
+      it "should return true" do
+        expect(subject.is_within_table?([0,5])).to be false
+      end
+      it "should return true" do
+        expect(subject.is_within_table?([0,9])).to be false
+      end
+      it "should return true" do
+        expect(subject.is_within_table?([-1,3])).to be false
+      end
+      it "should return true" do
+        expect(subject.is_within_table?([-1,-3])).to be false
+      end
+    end
+
+    context "check valid place command" do
+      it "should return true" do
+        expect(subject.is_valid_place?(['PLACE', 0, 0, 'NORTH'])).to be true
+      end
+      it "should return true" do
+        expect(subject.is_valid_place?(['PLACE', 0, 4, 'EAST'])).to be true
+      end
+      it "should return true" do
+        expect(subject.is_valid_place?(['PLACE', 1, 1, 'WEST'])).to be true
+      end
+      it "should return true" do
+        expect(subject.is_valid_place?(['PLACE', 4, 4, 'NORTH'])).to be true
+      end
+      it "should return true" do
+        expect(subject.is_valid_place?(['PLACE', 0, 0, 'NORTH-WEST'])).to be false
+      end
+      it "should return true" do
+        expect(subject.is_valid_place?(['PLACE', -2, -1, 'EAST'])).to be false
+      end
+      it "should return true" do
+        expect(subject.is_valid_place?(['PLACE', 3, 5, 'EAST'])).to be false
+      end
+      it "should return true" do
+        expect(subject.is_valid_place?(['PLACE', 2, -2, 'NORTH'])).to be false
+      end
+    end
+
   end
 end
