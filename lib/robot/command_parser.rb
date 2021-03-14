8bdbbfbabead587d98b28ec31d9aeeed297784d0
Renjith Vincent <renjithkv123@gmail.com>
2021-03-14 23:59:58 +0800

    Create command parser methods and tests

--- lib/robot/command_parser.rb
+++ lib/robot/command_parser.rb
@@ -1,5 +1,41 @@
 module RobotSimulator
   class CommandParser
+    attr_accessor :table
 
+    def initialize(options = {})
+      @table = RobotSimulator::Table.new
+    end
+    def parse_commands(command_array)
+      command_array = command_array.select{|arr| arr.length > 0}
+      return command_array if command_array.empty?
+      command_array = command_array.map do |comm|
+        comm =  parse_place_command(comm) if comm.first.index(table.place_command)
+        comm
+      end
+      filtered_command_array = filter_valid_commands(command_array)
+    end
+
+    private
+
+    def filter_valid_commands(command_array)
+      command_array = command_array.select do |arr|
+        next unless table.is_valid_action? arr.first
+        if arr.first == table.place_command
+          next unless table.is_valid_place?(arr)
+        end
+        true
+      end
+      place_index = get_first_place_index(command_array)
+      return [] unless place_index
+      command_array = command_array[place_index..-1]
+    end
+
+    def get_first_place_index(command_array)
+      command_array.map(&:first).index(table.place_command)
+    end
+
+    def parse_place_command(place_command)
+      place_command.first.split(/[ ,]/).flatten.reject(&:empty?)
+    end
   end
 end
