4b0d6000fb9a9c22c1da26fcdd862f4424c6b43e
Renjith Vincent <renjithkv123@gmail.com>
2021-03-14 21:43:35 +0800

    Create robot functions and tests

--- lib/robot/robot.rb
+++ lib/robot/robot.rb
@@ -1,5 +1,61 @@
 module RobotSimulator
   class Robot
+    attr_reader :table, :coordinates
 
+    def initialize(options={})
+      @table = RobotSimulator::Table.new
+      @coordinates = Coordinates.new
+    end
+
+    def perform_robot_movement(action_array)
+      actions = {
+          'PLACE' => method(:place),
+          'MOVE' => method(:move),
+          'LEFT' => method(:turn_left),
+          'RIGHT' => method(:turn_right),
+          'REPORT' => method(:report),
+      }[action_array.first]
+    end
+
+    private
+
+    def place(place_array)
+      place_coordinates = place_array[1,2].map(&:to_i)
+      place_direction = place_array[3]
+      new_coordinates = place_coordinates << place_direction
+    end
+
+    def move(current_position)
+      new_position = get_new_position(current_position, :move_x, :move_y)
+    end
+
+    def turn_left(current_position)
+      new_position = turn(current_position,:left)
+    end
+
+    def turn_right(current_position)
+      new_position = turn(current_position,:right)
+    end
+
+    def report(current_positition)
+      puts  current_positition.join(',')
+      current_positition
+    end
+
+    def turn(current_position, turn_sym)
+      new_position = current_position.dup
+      direction = new_position[2]
+      new_position[2] = coordinates.get_coordinates(direction.to_sym, turn_sym)
+      new_position
+    end
+
+    def get_new_position(current_position, x_sym, y_sym)
+      position = current_position.dup
+      x, y, direction = position[0], position[1], position[2]
+      puts x, y, direction, coordinates.get_coordinates(direction.to_sym, x_sym)
+      position[0] = x + coordinates.get_coordinates(direction.to_sym, x_sym)
+      position[1] = y + coordinates.get_coordinates(direction.to_sym, y_sym)
+      table.is_within_table?(position[0..1]) ? position : current_position
+    end
   end
 end
--- spec/robot/robot_spec.rb
+++ spec/robot/robot_spec.rb
@@ -7,5 +7,22 @@ module RobotSimulator
         expect(Robot).to be RobotSimulator::Robot
       end
     end
+
+    subject {Robot.new}
+
+    context "#perform_robot_movement with valid input" do
+      it "should return new coordinates" do
+        expect(subject.perform_robot_movement(['PLACE']).call(['PLACE',0,1,'NORTH'])).to eq([0,1,'NORTH'])
+      end
+      it "should return new coordinates" do
+        expect(subject.perform_robot_movement(['MOVE']).call([0,1,'NORTH'])).to eq([0,2,'NORTH'])
+      end
+      it "should return new coordinates" do
+        expect(subject.perform_robot_movement(['LEFT']).call([1,1,'SOUTH'])).to eq([1,1,'EAST'])
+      end
+      it "should return new coordinates" do
+        expect(subject.perform_robot_movement(['RIGHT']).call([3,3,'WEST'])).to eq([3,3,'NORTH'])
+      end
+    end
   end
 end
